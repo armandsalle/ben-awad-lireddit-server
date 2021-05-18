84bd3a7ab72502f37913b69e2627736884cc3ada
Armand Sallé <armand.salle@gmail.com>
2021-05-18 22:41:07 +0200

    :sparkles: Add cors

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { buildSchema } from "type-graphql"
 import connectRedis from "connect-redis"
 import redis from "redis"
 import session from "express-session"
+import cors from "cors"
 
 import mikroOrmConfig from "./mikro-orm.config"
 import { HelloResolver } from "./resolvers/hello"
@@ -21,6 +22,13 @@ const main = async () => {
 
   const RedisStore = connectRedis(session)
   const redisClient = redis.createClient()
+
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  )
 
   app.use(
     session({
@@ -46,7 +54,7 @@ const main = async () => {
     context: ({ req, res }) => ({ em: orm.em, req, res }),
   })
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({ app, cors: false })
 
   app.listen(4000, () => {
     console.log("🚀 server running on port 4000")
