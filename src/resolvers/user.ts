4b82f1ad3f9f7f18a4bff8e6022fd51d369164c8
Armand Sallé <armand.salle@gmail.com>
2021-05-20 11:19:28 +0200

    Add logout mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,8 @@
 import { User } from "../entities/User"
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql"
 import argon2 from "argon2"
-import { MyContext } from "src/types"
+import { MyContext } from "../types"
+import { COOKIE_NAME } from "../constants"
 
 @InputType()
 class UsernamePasswordInput {
@@ -121,5 +122,22 @@ export class UserResolver {
     return {
       user,
     }
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME)
+
+        if (err) {
+          console.log(err)
+          resolve(false)
+          return
+        }
+
+        resolve(true)
+      })
+    )
   }
 }
