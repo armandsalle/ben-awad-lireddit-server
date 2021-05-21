1147ea0518fe32174bc9f2fd19a4fad12030f2cd
Armand Sallé <armand.salle@gmail.com>
2021-05-21 14:47:04 +0200

    Add post and relations

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,16 @@
 import { Post } from "../entities/Post"
-import { Arg, Int, Mutation, Query, Resolver } from "type-graphql"
+import { Arg, Int, Mutation, InputType, Query, Resolver, Field, Ctx, UseMiddleware } from "type-graphql"
+import { MyContext } from "src/types"
+import { isAuth } from "../middleware/isAuth"
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string
+
+  @Field()
+  text: string
+}
 
 @Resolver()
 export class PostResolver {
@@ -14,11 +25,13 @@ export class PostResolver {
   }
 
   @Mutation(() => Post)
-  async createPost(@Arg("title", () => String) title: string): Promise<Post> {
-    return Post.create({ title }).save()
+  @UseMiddleware(isAuth)
+  async createPost(@Arg("input", () => PostInput) input: PostInput, @Ctx() { req }: MyContext): Promise<Post> {
+    return Post.create({ ...input, creatorId: req.session.userId }).save()
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
     @Arg("id", () => Int) id: number,
     @Arg("title", () => String, { nullable: true }) title: string
@@ -37,6 +50,7 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean, { nullable: true })
+  @UseMiddleware(isAuth)
   async deletePost(@Arg("id", () => Int) id: number): Promise<Boolean> {
     try {
       await Post.delete(id)
